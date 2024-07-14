d46a7d2d1084ec943f4bbb0e725266176545122b
jessicazambranocg <jessi.zambrano18@gmail.com>
2024-07-14 10:24:40 -0400

    feat: finish excersice activelearningdynamicshoppinglist

---
+++ script.js
@@ -0,0 +1,24 @@
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  const currentItem = input.value;
+    input.value = "";
+
+const listItem = document.createElement("li");
+const listSpan = document.createElement("span");
+const listButton = document.createElement("button");
+
+listItem.appendChild(listSpan);
+listSpan.textContent = currentItem;
+listItem.appendChild(listButton);
+listButton.textContent = "Delete";
+list.appendChild(listItem);
+
+listButton.addEventListener("click", () => {
+  list.removeChild(listItem);
+});
+
+input.focus();
+});
